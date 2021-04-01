646b22c4fe674e9ba9ddf20bbcace0c033ce2ecb
CommanderPoe <meadow.poe@gmail.com>
2021-04-02 00:03:14 +0200

    Updating labs

--- dataV3_Lesson_2.7_lab/lab_2.7.sql
+++ dataV3_Lesson_2.7_lab/lab_2.7.sql
@@ -1,3 +1,5 @@
+USE sakila;
+
 -- 1. How many films are there for each of the categories in the category table. Use appropriate join to write this query.
 SELECT * FROM category;
 SELECT * FROM film_category;
@@ -19,74 +21,67 @@ USING (staff_id)
 WHERE spay.payment_date >= "2005-08-01" AND spay.payment_date <= "2005-08-31"
 GROUP BY name;
 
+-- 3. Which actor has appeared in the most films?
+SELECT * FROM actor;
+SELECT * FROM film_actor;
+SELECT * FROM film;
 
+SELECT CONCAT(sa.first_name,' ',sa.last_name) AS name, COUNT(title) as apariciones
+FROM sakila.actor AS sa
+JOIN sakila.film_actor AS sfa
+USING(actor_id)
+JOIN sakila.film
+USING(film_id)
+GROUP BY name
+ORDER BY apariciones DESC;
 
+-- 4. Most active customer (the customer that has rented the most number of films)
+SELECT * FROM customer;
+SELECT * FROM rental;
 
-
-
-
-
-USE sakila;
--- How many films are there for each of the categories in the category table. Use appropriate join to write this query.
-SELECT * 
-FROM sakila.film_category;
-SELECT c.category_id AS Category, COUNT(f.film_id) AS Nb_of_Films
-FROM sakila.film_category c   
-JOIN sakila.film f 
-USING (film_id)
-GROUP BY c.category_id;
--- Display the total amount rung up by each staff member in August of 2005.
-USE sakila;
-SELECT CONCAT(s.first_name,' ',s.last_name) AS Staff, SUM(p.amount) as Amount_in_August_2005
-FROM sakila.payment p
-JOIN sakila.staff as s
-ON (p.staff_id = s.staff_id)
-WHERE payment_date >= "2005-08-01" AND payment_date <= "2005-08-31"
-GROUP BY p.staff_id;
--- Which actor has appeared in the most films?
-SELECT CONCAT(a.first_name,' ',a.last_name) AS Actor_Actress, COUNT(f.film_id) as Nb_of_Films
-FROM sakila.film f
-JOIN sakila.film_actor fa
-USING (film_id)
-RIGHT JOIN sakila.actor a
-USING (actor_id)
-GROUP BY a.actor_id;
--- Most active customer (the customer that has rented the most number of films)
-SELECT CONCAT(a.first_name,' ',a.last_name) AS Actor_Actress, COUNT(f.film_id) as Nb_of_Films
-FROM sakila.film f
-JOIN sakila.film_actor fa
-USING (film_id)
-RIGHT JOIN sakila.actor a
-USING (actor_id)
-GROUP BY a.actor_id;
--- Most active customer (the customer that has rented the most number of films)
-SELECT CONCAT(c.first_name,' ',c.last_name) as Customer, COUNT(r.rental_id) as Video_Rented
-FROM sakila.customer c
-JOIN sakila.rental r
-USING (customer_id)
+SELECT customer_id, COUNT(customer_id)
+FROM rental
 GROUP BY customer_id
-ORDER BY Video_Rented DESC
-LIMIT 1;
--- Display the first and last names, as well as the address, of each staff member.
-SELECT CONCAT(s.first_name,' ',s.last_name) as Staff_Name, a.address as Address
-FROM sakila.staff s
-JOIN sakila.address a
-USING (address_id)
-GROUP BY Address;
--- List each film and the number of actors who are listed for that film.
-SELECT title as Title, COUNT(fa.actor_id) as Nb_of_Actors_per_each_Films
-FROM sakila.film_actor fa
-JOIN sakila.film f
-USING (film_id)
-RIGHT JOIN sakila.actor a
-USING (actor_id)
-GROUP BY Title;
+ORDER BY COUNT(customer_id) ASC;
+
+SELECT sc.customer_id, CONCAT(sc.first_name, ' ', sc.last_name) AS name, COUNT(rental_id) AS times_rented
+FROM sakila.customer as sc
+JOIN sakila.rental as sr
+USING(customer_id)
+GROUP BY sc.customer_id
+ORDER BY times_rented DESC;
+
+-- 5. Display the first and last names, as well as the address, of each staff member.
+SELECT * FROM staff;
+SELECT * FROM address;
+
+SELECT ss.address_id, CONCAT(ss.first_name, ' ', ss.last_name) AS name, sa.address
+FROM sakila.staff as ss
+JOIN sakila.address as sa
+USING(address_id);
+
+-- 6. List each film and the number of actors who are listed for that film.
+SELECT * FROM film;
+SELECT * FROM film_actor;
+
+SELECT film_id, COUNT(actor_id)
+FROM film_actor
+GROUP BY film_id;
+
+SELECT film_id, title, COUNT(actor_id) as actors
+FROM sakila.film as sf
+JOIN sakila.film_actor as sa
+USING(film_id)
+GROUP BY sf.film_id
+ORDER BY actors DESC;
+
 -- Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name.
 SELECT CONCAT(c.first_name,' ',c.last_name) AS Customer, SUM(p.amount) AS Paid_by_Customer
 FROM sakila.payment p
 JOIN sakila.customer c
 USING (customer_id)
 GROUP BY Customer;
+
 -- List number of films per category.
 SELECT c.category_id AS Category, COUNT(f.film_id) AS Nb_of_Films
 FROM sakila.film_category c   
